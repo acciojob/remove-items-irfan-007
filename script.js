d85e4aac8eb0b4090b15f510def7e2e67d2c4332
irfan <85334710+irfan-007@users.noreply.github.com>
2024-07-29 15:09:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let color = document.getElementById("colorSelect");
+let btn = document.querySelector("input");
+const arr = [];
+btn.addEventListener("click", (e) => {
+  for (let i of color) arr.push(i.value);
+  let index = arr.indexOf(color.value);
+  // console.log(index);
+  color.remove(index);
+});
